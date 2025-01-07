7689290e3bcb01020aef6f9a80bc271155773186
hejjack <hejjack1@gmail.com>
2025-01-07 21:02:34 +0100

    new predictions

---
+++ clean_paper/config_runners/meta_runner_predict_exp8.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#PBS -q gpu_dgx@pbs-m1.metacentrum.cz
+#PBS -l walltime=20:0:0
+#PBS -l select=1:ncpus=6:ngpus=1:mem=20gb
+#PBS -N predict_nist
+
+cd /storage/brno2/home/ahajek/Spektro/MassGenie/clean_paper
+source /storage/brno2/home/ahajek/miniconda3/bin/activate BARTtrainH100
+echo $CONDA_PREFIX
+./config_runners/run_predict_many_exp8.sh
+
+exit
